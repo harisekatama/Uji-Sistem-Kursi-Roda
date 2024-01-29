f505de086004beb8de84c701c8cb41b85595ae01
I Putu Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2024-01-30 04:14:10 +0700

    add: Create JsonAP.ino

---
+++ ESP32/JsonAP.ino
@@ -0,0 +1,45 @@
+#include <WiFi.h>
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "B300Lab_ESP32"; // Nama SSID untuk Access Point
+const char* password = "123123123"; // Password untuk Access Point
+
+WiFiServer server(80); // Menggunakan port 80 untuk koneksi
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // Mulai menghubungkan ke Access Point yang dibuat oleh ESP32
+  WiFi.softAP(ssid, password);
+  delay(100);
+
+  server.begin(); // Memulai server
+  Serial.println("Server started");
+}
+
+void loop() {
+  WiFiClient client = server.available(); // Cek koneksi dari client
+
+  if (client) { // Jika ada koneksi dari client
+    Serial.println("New client connected");
+    
+    while (client.connected()) {
+      while (client.available()) {
+        String receivedData = client.readStringUntil('\n');
+
+        DynamicJsonDocument doc(1024);
+        DeserializationError error = deserializeJson(doc, receivedData);
+
+        const char* arah = doc["arah"];
+      //const char* kecepatan = doc["kecepatan"];
+        Serial.println(arah);
+        client.print("1");
+      }
+    }
+    
+    client.stop(); // Hentikan koneksi client
+    Serial.println("Client disconnected");
+  }
+}
